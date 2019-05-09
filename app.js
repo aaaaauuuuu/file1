a9a328cb1f1b403ca7f72f17f9dd0fc18eeabf84
aya uezato <a.u.91leo@gmail.com>
2019-05-09 09:44:46 +0900

    新規作成

---
+++ app.js
@@ -0,0 +1,6 @@
+let $id = function(id) { return document.getElementById(id); }
+let $class = function(c, n) { n=n||0; return document.getElementsByClassName(c)[n]; }
+let $classes = function(c)  { return document.getElementsByClassName(c); }
+ 
+
+let SOUND_PATH = ""
